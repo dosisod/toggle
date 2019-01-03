6f0368800eb9af050d191499b8420f542e73a52e
dosisod <39638017+dosisod@users.noreply.github.com>
2019-01-02 23:07:35 -0800

    worked on toggle class

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
+#include <functional>
 #include <iostream>
 #include <string>
-#include "toggle2.cpp"
+#include "toggle.cpp"
 
-void run1() { std::cout << "ran 1"; } //functions to execute
-void run2() { std::cout << "ran 2"; }
+//toggle example
+void run1(std::string s) { std::cout << s << std::endl; }
+void run2(std::string s) { std::cout << s+s << std::endl; }
+void run3(std::string s) { std::cout << s+s+s << std::endl; }
+
+//toggle2 example
+void move_arm(bool open) { std::cout << "arm " << (open?"opened":"closed") << std::endl; }
 
 int main() { //exmaple use for toggler class/classes
-	toggle2* test=new toggle2(run1, run2);
+
+	toggle2<bool>* arm=new toggle2<bool>(
+		move_arm, //run the command
+		move_arm,
+		std::vector<bool>{true,false} //but invert the params
+	);
+
+	toggle<std::string>* other=new toggle<std::string>( //toggle between 3 (or more) functions
+		std::vector<std::function<void(std::string)>>{run1, run2, run3},
+		std::vector<std::string>{"hello", "hey", "hi"}
+	);
+	
 	std::string input="";
+	std::cout << "2 toggles \"toggle2\", 3 toggles \"toggle\"" << std::endl;
 	for (;;) {
-		std::cout << "press a key:";
+		std::cout << ">";
 		std::getline(std::cin, input);
+		
 		//toggler checks if condition is good, then runs the next function
-		test->run(input=="123");
+		arm->toggleIf(input=="2");
+		other->toggleIf(input=="3");
 	}
 	return 0;
 }
